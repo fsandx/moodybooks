54603418e5b158ff2f2971b7d86beee00f0ccc95
Fred Sand <fsandx@gmail.com>
2021-09-23 12:34:52 +1000

    Using word wheel

--- README.md
+++ README.md
@@ -18,3 +18,39 @@ Add column for wordcount, in result.
 How many words in total (and how many occurences) do we need in a review to be useful/reliable
 
 ## Visualisation 3: Percentage of books with occurences
+
+
+## List books in each mood genre
+
+
+## Data wrangling
+The 100 words I am using is originally arranged as a wheel with three layers.
+The inner core layer consists of 7 parent words, which all the other words are children, or grandchildren of.
+The hierarchical model suggest me to use json instead of csv for an accurate representation of the relationships.
+But I have choosen to simplify the model, and only register the occurences of any word, in one of the seven parent groups, which means
+I can stick to 2D csv data source, with the 7 parents as columns and the number of "hits" for each review, as rows.   
+
+
+# Context
+Counting the occurence of words doesn't say anything about the context. For example: we count the word "joy" in the sentence: "This is not joy".
+
+# Naive Bayesian applied to synonyms
+The research project that came up with the list of words was done by eliminating words that was semantically close to each other. 
+We could use a naive bayesian approach to examine how probabible it is that people would classify the word as a synonym to one of our predefined labels rather than to any else. 
+
+## Other data sources
+- what does normal readers write about the book, analyze comments in social media.
+- If we only use one single review as a source for the calssification it becomes a classification of the review, rather than the book. 
+- But that might be interesting for a literary magazine / website.
+- It can also be interesting to see how much a particular review differes from the clasification of the text in the actual book.
+
+## Other parameters than nr of word occurences that can help determine the classification
+- popularity among people with certain demographic qualities
+- written by an author that often writes in the space
+- quoted and referenced to by those with an over weighing interest in that particular mood.
+- book is more read/bought (and borrowed from libraries) during special times of the year.
+
+## Conclusion
+- IWith the respect to Moddy Books's business model, I would recommend one, or borth of the below paths: 
+1) Increase hits by adding synonyms and exlude suffixes, additional fetch comments about the book from social media.
+2) Examine the best way to implement a rating system where the above used word classification can be used.
--- datawrangling.py
+++ datawrangling.py
@@ -1,14 +1,20 @@
-import os
-import pandas as pd
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-REVIEWS_PATH = 'data/reviews.json'
-WORDS_PATH = 'data/moodywords.csv'
-RESULT_PATH = 'data/moodywords_result.csv'
-
 # Run in IPython console:
 # import datawrangling as dw
 # dw.WordCounter()
+
+import os
+import pandas as pd
+
+REVIEWS_PATH = 'data/reviews.json'
+RESULT_PATH = 'data/moodywords_result.csv'
+
+WORDS_HAPPY = ['happy','playful', 'aroused', 'cheeky', 'content', 'free', 'joyful', 'interested', 'curious', 'inquisitive', 'proud', 'successful', 'confident', 'accepted', 'respected', 'valued', 'powerful', 'courageous', 'creative', 'peaceful', 'loving', 'thankful', 'trusting', 'sensitive', 'intimate', 'optimistic', 'hopeful', 'inspired']
+WORDS_SAD = ['sad', 'lonely', 'isolated', 'abandoned', 'vulnerable', 'victimized', 'fragile', 'despair', 'grief', 'powerlesss', 'hurt', 'embarrassed', 'depressed', 'inferior', 'empty', 'guilty', 'ashamed', 'remorseful']
+WORDS_DISGUSTED = ['disgusted', 'disapproving', 'judgemental', 'disappointed', 'appalled', 'revolted', 'awful', 'nauseated', 'detestable', 'repelled', 'hesitant', 'angry']
+WORDS_ANGRY = ['let down', 'betrayed', 'humiliated', 'disrespected', 'ridiculed', 'bitter', 'indignant', 'violated', 'mad', 'furious', 'jealous', 'aggressive', 'provoked', 'hostile', 'frustrated', 'infuriated', 'annoyed', 'distant', 'withdrawn', 'numb', 'critical', 'skeptical', 'dismissive']
+WORDS_FEARFUL = ['fearful', 'scared', 'helpless', 'frightened', 'anxious', 'overwhelmed', 'worried', 'insecure', 'inadequate', 'inferior', 'weak', 'worthless', 'insignificant', 'rejected', 'excluded', 'persecuted', 'threatened', 'nervous', 'exposed']
+WORDS_BAD = ["bad","bored","busy","stressed","tired","indifferent","apathetic","pressured","rushed","out of control","sleepy","unfocused"]
+WORDS_SURPRISED = ["surprised","startled","confused","amazed","excited","shocked","dismayed","disillusioned","perplexed","astonished",'awe',"eager","energetic"]
 
 class WordCounter:
 
@@ -18,12 +24,13 @@ class WordCounter:
 
     def loadData(self):
         self.reviews = pd.read_json(REVIEWS_PATH)
-        self.words = pd.read_csv(WORDS_PATH)
 
     def countWords(self):
-        
-        #Creating a list of all the moody words, which is the the fieldnames
-        word_fields = list(self.words)
+        #Create a single list of all words
+        words = [WORDS_HAPPY, WORDS_SAD,WORDS_DISGUSTED, WORDS_ANGRY, WORDS_FEARFUL, WORDS_BAD, WORDS_SURPRISED]
+
+        #Creating a list of all the first words, which will be the fieldnames
+        word_fields = [words[0][0], words[1][0],words[2][0], words[3][0], words[4][0], words[5][0], words[6][0]]
         
         #initializing a pandas dataframe
         self.counted = pd.DataFrame(columns = word_fields )
@@ -37,20 +44,18 @@ class WordCounter:
             # initialize the data
             data = [str(row['number']), str(len(rwords)) ]
 
-            # initialize the data fields
+            # initialize additional fields
+            columns = ['id', 'words']
 
-            columns = ['Book Id', 'Text Size']
-            
             # find occurences of "moody" words in the review
-            for mw in self.words:
-                
-                # counting occurences of the word
-                c = rwords.count(mw)
-                
-                # only adding the number of hits if they are more than 0
-                if (c > 0):
-                    data.append(str(c))
-                    columns.append(mw)
+            for wlist in words:
+                count = 0
+                for w in wlist:
+                    # counting occurences of the word
+                    count = count + rwords.count(w)
+                #adding the number of hits
+                data.append(str(count))
+                columns.append(wlist[0])
 
             # Creating a Pandas Serie (record/row) of the data
             s = pd.Series(data=data, index=columns)
